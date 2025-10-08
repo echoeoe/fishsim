540beb0ea8cbce52ec0742aaba6efecf4f11a207
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-10-08 00:40:17 -0400

    Added guidelines section to help players. Feeding triggers nitrate addition, testing with set amount. Added gallons var.

--- fishsim.js
+++ fishsim.js
@@ -4,6 +4,7 @@ var satiety = 1;
 var ammonia = 0;
 var nitrite = 0;
 var nitrate = 0;
+var gallons = 40;
 
 document.getElementById('day').innerHTML = day;
 document.getElementById('health').innerHTML = health;
@@ -11,24 +12,30 @@ document.getElementById('satiety').innerHTML = satiety;
 document.getElementById('ammonia').innerHTML = ammonia;
 document.getElementById('nitrite').innerHTML = nitrite;
 document.getElementById('nitrate').innerHTML = nitrate;
+document.getElementById('gallons').innerHTML = gallons;
 
 function feed(){
     //increase satiety - limited to once per day
     //feeding increases satiety, disables feed button 
     if(satiety<3){
         satiety++;
-        document.getElementById("satiety").innerHTML = satiety;
+        document.getElementById('satiety').innerHTML = satiety;
     }
     //disable
-    document.getElementById("feed-btn").disabled = true;
-    //adjust water quality
+    document.getElementById('feed-btn').disabled = true;
+    //adjust water quality: assume 100g (small) goldfish, based on 40 gal
+    // 2 mg/L nitrate end of day 
+    // 0.45 mg/L immediate ammonia, simplify to 0 by end of day 
+    // 0.05 - 0.1 mg/L nitrite brief transient spike - realism 
+    nitrate += 2;
+    document.getElementById('nitrate').innerHTML = nitrate;
 
 }
 
 function waterchange(changed){
     //adjust parameters ammonia, nitrite, nitrate based on tank size, amount changed. too much fluctuation - lower health 
     //ask how many gallons to change
-    let gallons = prompt("How many gallons to change?");
+    let gallons = prompt('How many gallons to change?');
 }
 
 function forward(){
@@ -36,5 +43,5 @@ function forward(){
     day++;
     document.getElementById('day').innerHTML = day;
     //re-enable feed button
-    document.getElementById("feed-btn").disabled = false;
+    document.getElementById('feed-btn').disabled = false;
 }
