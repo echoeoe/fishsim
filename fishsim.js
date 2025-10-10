fde791af0b0387efb29ad55d65e04280975d478a
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-10-10 00:50:50 -0400

    Made waterchange a function of tank for OOP design. Added fish facing right asset.

--- fishsim.js
+++ fishsim.js
@@ -9,21 +9,34 @@ class tank{
         this.nitrate = nitrate;
         this.liters = this.gal * 3.78541;
     }
+    waterchange(){
+    //adjust parameters ammonia, nitrite, nitrate based on tank size, amount changed. too much fluctuation - lower health 
+    //ask how many gallons to change
+    var galChange = prompt('How many gallons to change?');
+    //get proportion remaining, to multiply nitrates by
+    if (!isNaN(galChange)){
+        var remainProp = 1 - (galChange / this.gal); 
+        this.nitrate = remainProp * this.nitrate;
+        document.getElementById('nitrate').innerHTML = this.nitrate.toFixed(2);
+    }
+}
 }
 
 class fish{
-    constructor(name, health=100, satiety=1){
+    constructor(name, health=100, weight=100, satiety=1){
         this.name = name;
         this.health = health;
         this.satiety = satiety; 
         this.fedToday = false;
+        this.weight = weight;
+        this.foodGrams = weight * .01;
     }
 }
 
 const myFish = new fish("Sashimi");
 const myTank = new tank();
 
-var nitratePerFoodDay = nitratePerFoodGram / myTank.liters;
+var nitratePerFoodDay = (nitratePerFoodGram * myFish.foodGrams) / myTank.liters;
 
 document.getElementById('day').innerHTML = day;
 document.getElementById('health').innerHTML = myFish.health;
@@ -51,19 +64,6 @@ function feed(){
     myTank.nitrate = myTank.nitrate + nitratePerFoodDay;
     document.getElementById('nitrate').innerHTML = myTank.nitrate.toFixed(2);
 
-}
-
-function waterchange(){
-    //adjust parameters ammonia, nitrite, nitrate based on tank size, amount changed. too much fluctuation - lower health 
-    //ask how many gallons to change
-    var galChange = prompt('How many gallons to change?');
-    //get proportion remaining, to multiply nitrates by
-    if (!isNaN(galChange)){
-        var remainProp = 1 - (galChange / myTank.gal); 
-        myTank.nitrate = remainProp * myTank.nitrate;
-        document.getElementById('nitrate').innerHTML = myTank.nitrate.toFixed(2);
-    }
-    
 }
 
 function forward(){
